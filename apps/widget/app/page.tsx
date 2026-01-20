6272a12df2a2b9e1ff3d475a836f3eb2ad190dfa
Josh Kenyon <joshuakenyon1@gmail.com>
2026-01-20 07:18:03 -0300

    09: widget layout

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import { useMutation, useQuery } from "convex/react";
-import { api } from "@workspace/backend/_generated/api";
-import { NextPage } from "next";
-import { Button } from "@workspace/ui/components/button";
+import { WidgetView } from "@/modules/widget/ui/views/widget-view";
+import { use } from "react";
 
-const Page: NextPage = () => {
-  const users = useQuery(api.users.getMany);
-  const addUser = useMutation(api.users.add);
-
-  return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
-      <h1 className="text-2xl font-bold"> app/widget</h1>
-      <Button onClick={() => addUser()}>Add</Button>
-      <div className="max-w-sm w-full mx-auto">
-        {JSON.stringify(users, null, 2)}
-      </div>
-    </div>
-  );
+interface Props {
+  searchParams: Promise<{
+    organizationId: string;
+  }>
 }
 
-export default Page
+const Page = ({searchParams}: Props) => {
+  const organizationId = use(searchParams).organizationId
+
+
+
+  return <WidgetView  organizationId={organizationId}/>;
+};
+
+export default Page;
