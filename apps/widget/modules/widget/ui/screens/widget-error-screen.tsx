77de9460b54a08b197ce41e6c4795afcfe1bad45
Josh Kenyon <joshuakenyon1@gmail.com>
2026-02-01 17:45:08 -0300

    changed welcome screen text

--- apps/widget/modules/widget/ui/screens/widget-error-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-error-screen.tsx
@@ -14,7 +14,7 @@ export const WidgetErrorScreen = () => {
       <WidgetHeader>
         <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
           <p className=" text-3xl">Hi there! 👋</p>
-          <p className="text-lg">Let&apos;s get you started</p>
+          <p className="text-lg">How can I help you today?</p>
         </div>
       </WidgetHeader>
       <div className="flex flex-1 flex-col items-center justify-center gap-y-4 p-4 text-muted-foreground">
--- apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
+++ apps/widget/modules/widget/ui/screens/widget-selection-screen.tsx
@@ -59,7 +59,7 @@ export const WidgetSelectionScreen = () => {
       <WidgetHeader>
         <div className="flex flex-col justify-between gap-y-2 px-2 py-6 font-semibold">
           <p className=" text-3xl">Hi there! 👋</p>
-          <p className="text-lg">Let&apos;s get you started</p>
+          <p className="text-lg">How can I help you today?</p>
         </div>
       </WidgetHeader>
       <div className="flex flex-1 flex-col gap-y-4 p-4 overflow-y-auto">
